d00de681225e9005df67ae0a59edbb4b6045b7a8
Denis <31967091+denied5@users.noreply.github.com>
2018-10-20 22:20:23 +0300

    1.06

--- script.php
+++ script.php
@@ -1,10 +1,10 @@
 <?php 
-$upload_dir = 'uploads';  //implement this function yourself
+$upload_dir = 'uploads/';  //implement this function yourself
 $img = $_POST['imgBase64'];
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
-$file = $upload_dir."uploads/image_name.png";
+$file = $upload_dir."image_name.png";
 $success = file_put_contents($file, $data);
 var_dump($upload_dir, $img, $file, $success);
 
