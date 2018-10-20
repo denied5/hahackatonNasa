b2dd795d6680265ab95674cab60ec4f2714582ee
Denis <31967091+denied5@users.noreply.github.com>
2018-10-20 23:39:20 +0300

    1.08

--- script.php
+++ script.php
@@ -4,8 +4,8 @@ $img = $_POST['imgBase64'];
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
-$file = $upload_dir."image_name.png";
-chmod("/uploads/image_name.png", 0777);
+$file = $upload_dir."image_name1.png";
+chmod("/uploads/image_name1.png", 0777);
 $success = file_put_contents($file, $data);
 var_dump($upload_dir, $img, $file, $success);
 
