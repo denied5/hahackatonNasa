37671c1c9c7a6529593c34e87b5d49390ef19cad
Yui-Ezic <mishan221199@gmail.com>
2018-10-21 11:52:03 +0300

    wef

--- script.php
+++ script.php
@@ -14,7 +14,7 @@ $sourceImageUrl = "https://nasa.medispark.io/"  . $file;
 //echo $sourceImageUrl;
 define( 'API_BASE_URL',     'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect' );
 define( 'API_PRIMARY_KEY',      '04602a602bb543738e53391b304c1381' );
-$img = $sourceImageUrl;
+$img = "https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg";//$sourceImageUrl;
 
 $post_string = '{"url":"' . $img . '"}';
 
@@ -47,69 +47,27 @@ curl_close( $ch );
 
 $decode = json_decode($response, true);
 var_dump($decode[0]["faceAttributes"]["emotion"]);
+$emotion = $decode[0]["faceAttributes"]["emotion"]; 
 
+## Добавление записи в бд
+$host="localhost";
+$user="nasa";
+$pass= "AKN7n82l3N8n6ifC"; //установленный вами пароль
+$db_name="nasa";
 
+$link=mysql_connect($host,$user,$pass);
+mysql_select_db($db_name,$link);
 
+$nameAstr = $_POST['astrName'];
+$time = substr($name, 0, strpos($name, '.'));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-exit;
-$sourceImageUrl = "https://nasa.medispark.io/uploads/image_name.png";
-$params = [
-            "returnFaceId" => "true",
-            "returnFaceLandmarks"=> "false",
-            "returnFaceAttributes" => "emotion"
-];
-
-$regUrl = "https://nasa.medispark.io/uploads/1540079058060.png";
-$data = '{"url": ' . '"' . $sourceImageUrl . '"}';
-                       
-$data = ['data'=>["url" => $sourceImageUrl]];                                
-$data_string = json_encode($data); 
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $regUrl); //урл сайта к которому обращаемся
-	curl_setopt($curl, CURLOPT_POST, 1); //передача данных методом POST
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //теперь curl вернет нам ответ, а не выведет
-	
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    "Ocp-Apim-Subscription-Key: 04602a602bb543738e53391b304c1381",
-));
-	
-	curl_setopt($curl, CURLOPT_POSTFIELDS, //тут переменные которые будут переданы методом POST
-	$data_string);
-	
-	$res = curl_exec($curl);
-var_dump($res);
-	//если ошибка то печатаем номер и сообщение
-	if(!$res) {
-		$error = curl_error($curl).'('.curl_errno($curl).')';
-		echo $error;
-	}
-	else {
-		//если результат содержит то что нам надо (проверяем регуляркой), а в данном случае это табличка с классом yaResultat, то выводим ее.
-		if (preg_match("/\<table class\='yaResultat'(.+)\<\/table\>/isU", $res, $found)) {
-			$content = $found[0];
-			echo $content; //перед этим его конечно можно обработать всякими str_replace и т.д.
-		}
-		else {
-			echo "<p>Неизвестная ошибка</p>"; //а если табличики с результатами нет, то печатать нечего и мы незнаем что делать :(
-		}
-	}
-curl_close($curl);
-
-var_dump($upload_dir, $img, $file, $success);
+//Вставляем данные, подставляя их в запрос
+$sql = mysql_query("INSERT INTO `statistics` (`name`, `time`, `anger`, `contempt`, `disgust`, `fear`, `happiness`, `neutral`, `sadness`, `surprise`) VALUES ('".$nameAstr."','".$time."', '".$emotion["anger"]."', '".$emotion["contempt"]."', '".$emotion["disgust"]."', '".$emotion["fear"]."', '".$emotion["happiness"]."', '".$emotion["neutral"]."', '".$emotion["sadness"]."', '".$emotion["surprise"]."')");
+//Если вставка прошла успешно
+if ($sql) {
+    echo "<p>Данные успешно добавлены в таблицу.</p>";
+} else {
+    echo "<p>Произошла ошибка.</p>";
+}
 
 
