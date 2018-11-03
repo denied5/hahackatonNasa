2aa17b7417a7656b325e97703df5b7c6cdb092aa
Yui-Ezic <mishan221199@gmail.com>
2018-11-03 22:44:43 +0200

    Improve readability

--- index.php
+++ index.php
@@ -1,101 +1,101 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Detect Faces Sample</title>
-	<link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 </head>
+
 <body>
-  <div class="booth" >
-  	
-  	<form name="myForm">
-      <p style="display: inline-block;">1.31</p>
-      <input type="radio" name="astrName" checked="checked" value="Armstrong" /><span>Armstrong</span>
-      <input type="radio" name="astrName" value="Luis" /><span>Luis</span>
-      <input type="radio" name="astrName" value="Jhon" /><span>Jhon</span>
-       <input type="radio" name="astrName" value="Bob" /><span>Bob</span>
-    </form>
-    <div id="printBlock"></div>
+    <div class="booth">
 
-   <video id="video" width="400" height="300" autoplay></video>
-   <a href="#" id="capture" class="push_button red">Сфотографировать</a>
-   <canvas id="canvas" width="400" height="300"></canvas>
-   <img src="http://goo.gl/qgUfzX" id="photo" alt="Ваша фотография">
-  </div>
-  <script src="photo.js"></script>
+        <form name="myForm">
+            <p style="display: inline-block;">1.31</p>
+            <input type="radio" name="astrName" checked="checked" value="Armstrong" /><span>Armstrong</span>
+            <input type="radio" name="astrName" value="Luis" /><span>Luis</span>
+            <input type="radio" name="astrName" value="Jhon" /><span>Jhon</span>
+            <input type="radio" name="astrName" value="Bob" /><span>Bob</span>
+        </form>
+        <div id="printBlock"></div>
 
-<script type="text/javascript">
-  var AstronautName = "Armstrong";
-function onclick(e){
-    AstronautName = e.target.value;
-}
-for (var i = 0; i < myForm.astrName.length; i++) {
-    myForm.astrName[i].addEventListener("click", onclick);
-}
-	var time = "";
-	(function() {
-  var video = document.getElementById('video'),
-   canvas = document.getElementById('canvas'),
-   context = canvas.getContext('2d'),
-   photo = document.getElementById('photo'),
-   vendorUrl = window.URL || window.webkitURL;
-  navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.
-mozGetUserMedia || navigator.msGetUserMedia;
-  navigator.getMedia({
-   video: true,
-   audio: false
-  }, function(stream) {
-   video.src = vendorUrl.createObjectURL(stream);
-   video.play();
-  }, function(error) {
-   alert('Ошибка! Что-то пошло не так, попробуйте позже.');
-
-  });
-  document.getElementById('capture').addEventListener('click', function() {
-   context.drawImage(video, 0, 0, 400, 300);
-   photo.setAttribute('src', canvas.toDataURL('image/png'));
-  var dataURL = canvas.toDataURL();
-  var d = new Date();
- 
-  time = d.getTime()+".png";
-  
-$.ajax({
-  type: "POST",
-  url: "script.php",
-  data: { 
-     imgBase64: dataURL,
-     imgName: time,
-     astrName: AstronautName
-  }
-}).done(function(o) {
-  
-  console.log(o);
- 
-  $("#responseTextArea").val(o);
-  console.log('saved'); 
-});
-
-  });
-  
-})();
-</script>
-
-<h1>Detect Faces:</h1>
-
-<div id="wrapper" style="width:600px; display:table; float: left; margin-left: 30px;">
-    <div id="jsonOutput" style="width:600px; display:table-cell;">
-        
-
-        <textarea id="responseTextArea" class="UIInput"
-                  style="width:580px; height:640px;"></textarea>
+        <video id="video" width="400" height="300" autoplay></video>
+        <a href="#" id="capture" class="push_button red">Сфотографировать</a>
+        <canvas id="canvas" width="400" height="300"></canvas>
+        <img src="http://goo.gl/qgUfzX" id="photo" alt="Ваша фотография">
     </div>
 
+    <script type="text/javascript">
+        var AstronautName = "Armstrong";
 
-    <div id="imageDiv" style="width:420px; display:none;">
-        Source image:<br><br>
+        function onclick(e) {
+            AstronautName = e.target.value;
+        }
+        for (var i = 0; i < myForm.astrName.length; i++) {
+            myForm.astrName[i].addEventListener("click", onclick);
+        }
+        var time = "";
+        (function() {
+            var video = document.getElementById('video'),
+                canvas = document.getElementById('canvas'),
+                context = canvas.getContext('2d'),
+                photo = document.getElementById('photo'),
+                vendorUrl = window.URL || window.webkitURL;
+            navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.
+            mozGetUserMedia || navigator.msGetUserMedia;
+            navigator.getMedia({
+                video: true,
+                audio: false
+            }, function(stream) {
+                video.src = vendorUrl.createObjectURL(stream);
+                video.play();
+            }, function(error) {
+                alert('Ошибка! Что-то пошло не так, попробуйте позже.');
+                console.log("The following error occurred: " + error.name);
 
-        <img id="sourceImage" width="400" />
+            });
+            document.getElementById('capture').addEventListener('click', function() {
+                context.drawImage(video, 0, 0, 400, 300);
+                photo.setAttribute('src', canvas.toDataURL('image/png'));
+                var dataURL = canvas.toDataURL();
+                var d = new Date();
+
+                time = d.getTime() + ".png";
+
+                $.ajax({
+                    type: "POST",
+                    url: "script.php",
+                    data: {
+                        imgBase64: dataURL,
+                        imgName: time,
+                        astrName: AstronautName
+                    }
+                }).done(function(o) {
+
+                    console.log(o);
+
+                    $("#responseTextArea").val(o);
+                    console.log('saved');
+                });
+
+            });
+
+        })();
+    </script>
+
+    <h1>Detect Faces:</h1>
+
+    <div id="wrapper" style="width:600px; display:table; float: left; margin-left: 30px;">
+        <div id="jsonOutput" style="width:600px; display:table-cell;">
+            <textarea id="responseTextArea" class="UIInput" style="width:580px; height:640px;"></textarea>
+        </div>
+
+
+        <div id="imageDiv" style="width:420px; display:none;">
+            Source image:<br><br>
+            <img id="sourceImage" width="400" />
+        </div>
     </div>
-</div>
 </body>
+
 </html>
--- script.php
+++ script.php
@@ -1,54 +1,68 @@
-<?php 
-$upload_dir = 'uploads/';  //implement this function yourself
-$img = $_POST['imgBase64'];
-$name = $_POST['imgName'];
-$img = str_replace('data:image/png;base64,', '', $img);
-$img = str_replace(' ', '+', $img);
-$data = base64_decode($img);
-$file = $upload_dir.$name;
+<?php   
+    // Директория для загрузки фотогрфий
+    $upload_dir = 'uploads/';  
 
-$success = file_put_contents($file, $data);
-$sourceImageUrl = "https://nasa.medispark.io/"  . $file;
+    // Фотография с веб-камеры
+    $img = $_POST['imgBase64'];
 
-//echo $sourceImageUrl;
-define( 'API_BASE_URL',     'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect' );
-define( 'API_PRIMARY_KEY',      '04602a602bb543738e53391b304c1381' );
-$img = $sourceImageUrl;
+    // Имя фотографии
+    $name = $_POST['imgName'];
 
-$post_string = '{"url":"' . $img . '"}';
+    // Сохраняем фотографию на сервер
+    $img = str_replace('data:image/png;base64,', '', $img);
+    $img = str_replace(' ', '+', $img);
+    $data = base64_decode($img);
+    $file = $upload_dir.$name;
+    $success = file_put_contents($file, $data);
+    //TODO: обработать ошибку записи на сервер
 
-$query_params = array(
-     "returnFaceId" => "true",
-            'returnFaceLandmarks' => 'false',
-            'returnFaceAttributes' => 'emotion'
-);
+    /*Отправка фотографии microsoft для обработки*/
 
-$params = '';
-foreach( $query_params as $key => $value ) {
-    $params .= $key . '=' . $value . '&';
-}
-$params .= 'subscription-key=' . API_PRIMARY_KEY;
+    // Ссылка на изображение
+    $sourceImageUrl = "https://nasa.medispark.io/"  . $file;
 
-$post_url = API_BASE_URL . "?" . $params;
+    // Формируем curl запрос
+    define( 'API_BASE_URL', 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect' );
+    define( 'API_PRIMARY_KEY', '04602a602bb543738e53391b304c1381' );
+    
+    $post_string = '{"url":"' . $sourceImageUrl . '"}';
+    
+    $query_params = array(
+        'returnFaceId' => 'true',
+        'returnFaceLandmarks' => 'false',
+        'returnFaceAttributes' => 'emotion'
+    );
+    
+    $params = '';
+    
+    foreach( $query_params as $key => $value ) {
+        $params .= $key . '=' . $value . '&';
+    }
+    
+    $params .= 'subscription-key=' . API_PRIMARY_KEY;
 
-$ch = curl_init();
+    $post_url = API_BASE_URL . "?" . $params;
+
+    $ch = curl_init();
     curl_setopt( $ch, CURLOPT_HTTPHEADER, array(                                                                          
-        'Content-Type: application/json',                                                                                
-        'Content-Length: ' . strlen($post_string))
+            'Content-Type: application/json',                                                                                
+            'Content-Length: ' . strlen($post_string))
     );    
-
     curl_setopt( $ch, CURLOPT_URL, $post_url );
     curl_setopt( $ch, CURLOPT_POST, true );
     curl_setopt( $ch, CURLOPT_POSTFIELDS, $post_string );
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    
+    // Получаем ответ
     $response = curl_exec( $ch );
-curl_close( $ch );
+    
+    curl_close( $ch );
 
-$decode = json_decode($response, true);
-foreach($decode as $people){ 
-$emotion = people["faceAttributes"]["emotion"];
-foreach ($emotion as $key => $value) {
-    echo "$key = $value \n";
-}
-}
-
+    // Обработка ответа
+    $decode = json_decode($response, true);
+    foreach($decode as $people){ 
+        $emotion = people["faceAttributes"]["emotion"];
+        foreach ($emotion as $key => $value) {
+            echo "$key = $value \n";
+        }
+    }  
