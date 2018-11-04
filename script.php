b37cce2ab174af85c31711e7e117a19918193f6c
Yui-Ezic <mishan221199@gmail.com>
2018-11-04 02:18:54 +0200

    Fix some problems

--- script.php
+++ script.php
@@ -20,10 +20,11 @@
 
     // Ссылка на изображение
     $sourceImageUrl = "https://nasa.medispark.io/"  . $file;
+    //$sourceImageUrl = "https://ak4.picdn.net/shutterstock/videos/2173574/thumb/1.jpg";
 
     // Формируем curl запрос
-    define( 'API_BASE_URL', 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect' );
-    define( 'API_PRIMARY_KEY', '04602a602bb543738e53391b304c1381' );
+    define( 'API_BASE_URL', 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect' );
+    define( 'API_PRIMARY_KEY', '3f7748279b184bb096349044a959737c' );
     
     $post_string = '{"url":"' . $sourceImageUrl . '"}';
     
@@ -56,13 +57,17 @@
     // Получаем ответ
     $response = curl_exec( $ch );
     
+    /*$response = ' [{"faceId":"46e48f46-60fc-4b87-9add-aecd9d684351","faceRectangle":{"top":117,"left":485,"width":93,"height":93},"faceAttributes":{"emotion":{"anger":0.0,"contempt":0.0,"disgust":0.0,"fear":0.0,"happiness":1.0,"neutral":0.0,"sadness":0.0,"surprise":0.0}}},{"faceId":"4b8de7c8-a93d-4fce-9dde-629067501c90","faceRectangle":{"top":120,"left":217,"width":84,"height":84},"faceAttributes":{"emotion":{"anger":0.0,"contempt":0.0,"disgust":0.0,"fear":0.0,"happiness":0.375,"neutral":0.624,"sadness":0.0,"surprise":0.0}}}]';*/
+
+    
     curl_close( $ch );
 
     // Обработка ответа
     $decode = json_decode($response, true);
-    foreach($decode as $people){ 
-        $emotion = people["faceAttributes"]["emotion"];
+    foreach ($decode as $number => $people){ 
+        $emotion = $people["faceAttributes"]["emotion"];
         foreach ($emotion as $key => $value) {
             echo "$key = $value \n";
         }
-    }  
+        echo "\n";
+    } 
